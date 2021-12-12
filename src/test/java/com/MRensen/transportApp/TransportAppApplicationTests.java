479efc6da7f068fc123908a549ce59a2d585c67a
MRensen <markrensen@hotmail.com>
2021-12-12 21:47:16 +0100

    initialized using Spring Initializr

---
+++ src/test/java/com/MRensen/transportApp/TransportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MRensen.transportApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
