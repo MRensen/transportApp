70084b94dae77d6d9534df73815c928ec2bcd46e
MRensen <markrensen@hotmail.com>
2022-01-27 15:20:15 +0100

    added endpointlogger

--- src/main/java/com/MRensen/transportApp/service/UserService.java
+++ src/main/java/com/MRensen/transportApp/service/UserService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
@@ -60,10 +59,6 @@ public class UserService {
         if (userOptional.isPresent()) {
             User user = userOptional.get();
             byte[] decodedImg = image.getBytes();
-//            String partSeparator = ",";
-//            if (image.contains(partSeparator)) {
-//                String encodedImg = image.split(partSeparator)[1];
-//                byte[] decodedImg = Base64.getEncoder().encode(image.getBytes(StandardCharsets.UTF_8));
                 user.setImage(decodedImg);
                 userRepository.save(user);
 //            }
